6449b066a2f0d8b146ec1d848f3e91916b375d0b
mindkeeper <fcb.nyak@gmail.com>
2023-09-15 14:14:38 +0700

    creating navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { useMemo } from "react";
+import {
+  CustomLink,
+  Logo,
+  LogoContainer,
+  LogoTitle,
+  Nav,
+  NavContainer,
+  NavLinkContainer,
+} from ".";
+import logo from "../assets/images/logo.png";
+import { useNavigate } from "react-router-dom";
+
+export function Navbar() {
+  const links = useMemo(
+    () => [
+      { label: "Top Up", path: "/top-up" },
+      { label: "Transaction", path: "/transaction" },
+      { label: "History", path: "/history" },
+      { label: "Akun", path: "/account" },
+    ],
+    []
+  );
+  const navigate = useNavigate();
+  return (
+    <Nav>
+      <NavContainer>
+        <LogoContainer
+          onClick={() => {
+            navigate("/home");
+          }}
+        >
+          <Logo src={logo} alt="logo" />
+          <LogoTitle>SIMS PPOB</LogoTitle>
+        </LogoContainer>
+        <NavLinkContainer>
+          {links.map((link, idx) => (
+            <CustomLink to={link.path} key={idx}>
+              {link.label}
+            </CustomLink>
+          ))}
+        </NavLinkContainer>
+      </NavContainer>
+    </Nav>
+  );
+}
