9c1640c6d61326a37fcf4073c165899ff0c70ac1
mindkeeper <fcb.nyak@gmail.com>
2023-09-15 17:09:25 +0700

    styling main layout

---
+++ src/components/styled/styled.main.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { theme } from "../../assets/styles";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  width: 100%;
+`;
+
+export const MainWrapper = styled.div`
+  padding-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  width: 90%;
+  margin: 0 auto;
+`;
+
+export const UserSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 40vh;
+
+  @media (${theme.breakPoint.tabletL}) {
+    align-items: center;
+    flex-direction: row;
+    height: 20vh;
+  }
+`;
