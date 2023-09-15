38c5317fef81ad79e17cd59e5a70a66acb1d6107
mindkeeper <fcb.nyak@gmail.com>
2023-09-15 13:51:58 +0700

    creating css variables

---
+++ src/assets/styles/variables.js
@@ -0,0 +1,13 @@
+import { css } from "styled-components";
+
+export const variables = css`
+  :root {
+    --bg-primary: #eb3b13;
+    --primary: #eb3b13;
+    --secondary: #6497b1;
+    --font-primary: #343434;
+    --font-secondary: #2c2727;
+    --green: #4bb543;
+    --white-grey: #ccc;
+  }
+`;
