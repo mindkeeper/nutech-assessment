b9a8063c05d985e7990fc918e59430dd9b50322f
mindkeeper <fcb.nyak@gmail.com>
2023-09-15 13:51:47 +0700

    creating breakpoint

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  breakPoint: {
+    mobileS: `min-width: 330px`,
+    mobileM: `min-width: 400px`,
+    mobileL: `min-width: 480px`,
+    tabletS: `min-width: 600px`,
+    tabletL: `min-width: 768px`,
+    desktopXS: `min-width: 900px`,
+    desktopS: `min-width: 1080px`,
+    desktopM: `min-width: 1200px`,
+    desktopL: `min-width: 1400px`,
+  },
+};
