c250792690fd9f672509e093ca57ab6bce36c9a9
mindkeeper <fcb.nyak@gmail.com>
2023-09-15 14:25:26 +0700

    creating auth layout

---
+++ src/app/auth/components/Layout.jsx
@@ -0,0 +1,42 @@
+import {
+  Banner,
+  BannerContainer,
+  Container,
+  FormContainer,
+  FormContent,
+  FormWrapper,
+  Logo,
+  LogoContainer,
+  LogoTitle,
+  Title,
+  Wrapper,
+} from "./styled";
+import { useLocation } from "react-router-dom";
+import authImage from "../../../assets/images/auth-image.png";
+import logo from "../../../assets/images/logo.png";
+export function _Layout({ children }) {
+  const param = useLocation();
+  return (
+    <Container>
+      <Wrapper>
+        <FormContainer>
+          <LogoContainer>
+            <Logo src={logo} alt="logo" />
+            <LogoTitle>SIMS PPOB</LogoTitle>
+          </LogoContainer>
+          <Title>
+            {param.pathname === "/login"
+              ? "Masuk atau buat akun untuk memulai"
+              : "Lengkapi data untuk membuat akun"}
+          </Title>
+          <FormWrapper>
+            <FormContent>{children}</FormContent>
+          </FormWrapper>
+        </FormContainer>
+        <BannerContainer>
+          <Banner src={authImage} alt="banner" />
+        </BannerContainer>
+      </Wrapper>
+    </Container>
+  );
+}
